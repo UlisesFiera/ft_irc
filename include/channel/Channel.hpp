42f5b7a2dc87f6d0efbc6ebf5363fe741dca2798
Alvaro S <iphonedealvarosalguero@gmail.com>
2026-07-17 17:33:56 +0200

    first version of the class Channel

---
+++ include/channel/Channel.hpp
@@ -0,0 +1,50 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Channel
+{
+	public:
+		Channel();
+		Channel(const std::string &name);
+		Channel(const Channel &other);
+		Channel &operator=(const Channel &other);
+		~Channel();
+
+		std::string			getName() const;
+		std::vector<int>	getMembers() const;
+		bool    			getInviteOnly() const;
+		bool   				getTopicRestricted() const;
+		std::string			getPassword() const;
+		size_t				getUserLimit() const;
+		std::vector<int>	getInviteFDs() const;
+		std::vector<int>	getOperatorFDs() const;
+
+		void    			setName(std::string name);
+		void    			setMembers(std::vector<int> members);
+		void    			setInviteOnly(bool invite);
+		void    			setTopicRestricted(bool restricted);
+		void    			setPassword(std::string password);
+		void    			setUserLimit(size_t limit);
+		void				setInviteFDs(std::vector<int> inviteFDs);
+		void				setOperatorFDs(std::vector<int> operatorFDs);
+
+		bool				checkName(const std::string &name) const;
+		bool    			checkPassword(const std::string &password) const;
+
+	private:
+		std::string			_name;
+		std::vector<int>	_members;
+		bool                _inviteOnly;      // Modo +i
+		bool                _topicRestricted; // Modo +t
+		std::string         _password;        // Modo +k (vacío si no hay)
+		size_t              _userLimit;       // Modo +l (0 significa sin límite)
+		std::vector<int>    _invitedFDs;      // Lista blanca para el modo +i
+		std::vector<int>    _operatorFDs;     // Lista de administradores del canal (Modo +o)
+};
+
+#endif
---
+++ src/channel/Channel.cpp
@@ -0,0 +1,158 @@
+#include "channel/Channel.hpp"
+
+Channel::Channel () {}
+
+Channel::Channel (const std::string &name) : _inviteOnly(false), _topicRestricted(false), _userLimit(0)
+{
+	try
+	{
+		if (checkName(name) == true)
+			_name = name;
+		else
+			throw std::invalid_argument("[ERROR]: Invalid Channel name.");
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+Channel::Channel (const Channel &other)
+{
+	_name = other._name;
+	_members = other._members;
+	_inviteOnly = other._inviteOnly;
+	_topicRestricted = other._topicRestricted;
+	_password = other._password;
+	_userLimit = other._userLimit;
+	_invitedFDs = other._invitedFDs;
+	_operatorFDs = other._operatorFDs;
+}
+
+Channel &Channel::operator=(const Channel &other)
+{
+	if (this != &other)
+	{
+		_name = other._name;
+		_members = other._members;
+		_inviteOnly = other._inviteOnly;
+		_topicRestricted = other._topicRestricted;
+		_password = other._password;
+		_userLimit = other._userLimit;
+		_invitedFDs = other._invitedFDs;
+		_operatorFDs = other._operatorFDs;
+	}
+	return *this;
+}
+
+Channel::~Channel() {}
+
+
+std::string	Channel::getName() const
+{
+	return _name;
+}
+
+std::vector<int> Channel::getMembers() const
+{
+	return _members;
+}
+
+bool Channel::getInviteOnly() const
+{
+	return _inviteOnly;
+}
+
+bool Channel::getTopicRestricted() const
+{
+	return _topicRestricted;
+}
+
+std::string Channel::getPassword() const
+{
+	return _password;
+}
+
+size_t Channel::getUserLimit() const
+{
+	return _userLimit;
+}
+
+std::vector<int> Channel::getInviteFDs() const
+{
+	return _invitedFDs;
+}
+
+std::vector<int> Channel::getOperatorFDs() const
+{
+	return _operatorFDs;
+}
+
+
+
+
+void Channel::setName(std::string name)
+{
+	_name = name;
+}
+
+void Channel::setMembers(std::vector<int> members)
+{
+	_members = members;
+}
+
+void Channel::setInviteOnly(bool invite)
+{
+	_inviteOnly = invite;
+}
+
+void Channel::setTopicRestricted(bool restricted)
+{
+	_topicRestricted = restricted;
+}
+
+void Channel::setPassword(std::string password)
+{
+	_password = password;
+}
+
+void Channel::setUserLimit(size_t limit)
+{
+	_userLimit = limit;
+}
+
+void Channel::setInviteFDs(std::vector<int> inviteFDs)
+{
+	_invitedFDs = inviteFDs;
+}
+
+void Channel::setOperatorFDs(std::vector<int> operatorFDs)
+{
+	_operatorFDs = operatorFDs;
+}
+
+
+
+
+bool Channel::checkName(const std::string &name) const
+{
+	if (name.empty() || name[0] != '#')
+        return false;
+
+	for (size_t i = 0; i < name.size(); i++)
+	{
+		if ((name[i] >= 'a' && name[i] <= 'z') || (name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= '0' && name[i] <= '9') || name[i] == '_')
+			continue;
+		else
+			return false;
+	}
+	return true;
+}
+
+bool Channel::checkPassword(const std::string &password) const
+{
+	if (_password == password)
+		return true;
+	else
+		return false;
+}
